c77738b6fad7879baab25221ba14033e66a2e47d
suthirak <suthirakprom81@gmail.com>
2021-08-25 15:35:49 +0700

    80% done

--- crud_app/server.js
+++ crud_app/server.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');           // #2 research
 const bodyParser = require('body-parser');
 const path = require('path');
 
+const connectDB = require('./server/database/connection')
 const app = express()
 
 dotenv.config({path: 'config.env'})         // #1 need further research
@@ -11,6 +12,9 @@ const PORT = process.env.PORT || 8080
 
 // log request
 app.use(morgan('tiny'));                    // #2 research
+
+// mongodb
+connectDB();
 
 // parse request to body-parser
 app.use(bodyParser.urlencoded({extended: true}))
@@ -23,10 +27,9 @@ app.use('/css', express.static(path.resolve(__dirname, 'assets/css')))      // t
 app.use('/img', express.static(path.resolve(__dirname, 'assets/img')))      // to make your img works here
 app.use('/js', express.static(path.resolve(__dirname, 'assets/js')))        // to make your js works here
 
+// load router
+app.use('/', require('./server/routes/router'))
 
-app.get('/', (req, res) => {
-    res.render('index')
-})
 
 app.listen(PORT, () => {
     console.log(`Server lisening on port ${PORT}...`)
